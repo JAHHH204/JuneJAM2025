b51616188837bff79e41210bca242bde6083b8d3
LordTraxxas <167879743+LordTraxxas@users.noreply.github.com>
2025-06-24 16:11:12 -0400

    grey box for tutorial finished

--- Assets/Scripts/Managers/MainMenuScript.cs
+++ Assets/Scripts/Managers/MainMenuScript.cs
@@ -23,7 +23,7 @@ public class MainMenuScript : MonoBehaviour
     }
     public void StartGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("TutorialLevel");
         Debug.Log("Starting the game");
     }
 
